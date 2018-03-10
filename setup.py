ee02ea6a68f57affcc1e360e69c84504f79b3007
Jason R. Coombs <jaraco@jaraco.com>
2018-03-10 12:27:53 -0500

    Declare support for Python 3

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'License :: OSI Approved :: BSD License',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators'
